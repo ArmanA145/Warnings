bc5c6522bfe674e6395835d24a06287b4e1cb7cf
Arman A. <ArmanA145@users.noreply.github.com>
2015-04-03 12:58:43 -0600

    Update Main.java

--- src/main/java/me/arman/warnings/Main.java
+++ src/main/java/me/arman/warnings/Main.java
@@ -10,6 +10,13 @@ import me.arman.warnings.commands.TakeWarningsCommand;
 import me.arman.warnings.commands.WarnCommand;
 import me.arman.warnings.commands.WarningInfoCommand;
 import me.arman.warnings.commands.WarningsCommand;
+import me.arman.warnings.commands.categories.AddWarningsCommandCategories;
+import me.arman.warnings.commands.categories.CategoriesCommand;
+import me.arman.warnings.commands.categories.ClearWarningsCommandCategories;
+import me.arman.warnings.commands.categories.SetWarningsCommandCategories;
+import me.arman.warnings.commands.categories.TakeWarningsCommandCategories;
+import me.arman.warnings.commands.categories.WarnCommandCategories;
+import me.arman.warnings.commands.categories.WarningsCommandCategories;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -20,6 +27,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
 	public DataFile dFile;
+	Main plugin;
 
 	@Override
 	public void onEnable() {
@@ -33,6 +41,13 @@ public class Main extends JavaPlugin {
 
 	}
 
+	private void defaultConfigSetup() {
+		File config = new File(getDataFolder(), "config.yml");
+		if (!config.exists()) {
+			this.saveResource("config.yml", false);
+		}
+	}
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 		if (cmd.getName().equalsIgnoreCase("ReloadWarnings")) {
@@ -42,34 +57,59 @@ public class Main extends JavaPlugin {
 				return true;
 			}
 			reloadConfig();
+			registerCommands();
 			sender.sendMessage(ChatColor.GREEN + "Reloaded the configuration!");
 
 		}
 		return false;
 	}
 
-	private void defaultConfigSetup() {
-		File config = new File(getDataFolder(), "config.yml");
-		if (!config.exists()) {
-			this.saveResource("config.yml", false);
-		}
-	}
-
 	private void registerCommands() {
-		getCommand("Warn").setExecutor(new WarnCommand(this));
-		getCommand("Warnings").setExecutor(new WarningsCommand(this));
-		getCommand("ClearWarnings").setExecutor(new ClearWarningsCommand(this));
-		getCommand("ResetWarnings").setExecutor(new ResetWarningsCommand(this));
-		getCommand("SetWarnings").setExecutor(new SetWarningsCommand(this));
-		getCommand("AddWarnings").setExecutor(new AddWarningsCommand(this));
-		getCommand("TakeWarnings").setExecutor(new TakeWarningsCommand(this));
+		if (!(getConfig().getBoolean("categories.use"))) {
+			getCommand("Warn").setExecutor(new WarnCommand(this));
+		} else {
+			getCommand("Warn").setExecutor(new WarnCommandCategories(this));
+		}
+		if (!(getConfig().getBoolean("categories.use"))) {
+			getCommand("Warnings").setExecutor(new WarningsCommand(this));
+		} else {
+			getCommand("Warnings").setExecutor(
+					new WarningsCommandCategories(this));
+		}
+		if (!(getConfig().getBoolean("categories.use"))) {
+			getCommand("ClearWarnings").setExecutor(
+					new ClearWarningsCommand(this));
+		} else {
+			getCommand("ClearWarnings").setExecutor(
+					new ClearWarningsCommandCategories(this));
+		}
+		if (!(getConfig().getBoolean("categories.use"))) {
+			getCommand("SetWarnings").setExecutor(new SetWarningsCommand(this));
+		} else {
+			getCommand("SetWarnings").setExecutor(
+					new SetWarningsCommandCategories(this));
+		}
+		if (!(getConfig().getBoolean("categories.use"))) {
+			getCommand("AddWarnings").setExecutor(new AddWarningsCommand(this));
+		} else {
+			getCommand("AddWarnings").setExecutor(
+					new AddWarningsCommandCategories(this));
+		}
+		if (!(getConfig().getBoolean("categories.use"))) {
+			getCommand("TakeWarnings").setExecutor(
+					new TakeWarningsCommand(this));
+		} else {
+			getCommand("TakeWarnings").setExecutor(
+					new TakeWarningsCommandCategories(this));
+		}
 		getCommand("WarningInfo").setExecutor(new WarningInfoCommand(this));
+		getCommand("Categories").setExecutor(new CategoriesCommand(this));
+		getCommand("ResetWarnings").setExecutor(new ResetWarningsCommand(this));
 
 		if (getConfig().getBoolean("resetEnabled")) {
 			Bukkit.getScheduler().scheduleSyncRepeatingTask(
 					this,
 					new Runnable() {
-
 						public void run() {
 							for (String key : dFile.getConfig().getKeys(false)) {
 								dFile.getConfig().set(key, null);
@@ -83,4 +123,5 @@ public class Main extends JavaPlugin {
 
 		}
 	}
+
 }
